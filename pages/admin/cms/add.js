11c8e102dd99ef4b1d8197f27eb6c31662f42300
Francis Kim <francis@80bots.com>
2020-04-05 19:55:58 +1000

    remove pages for CMS

--- pages/admin/cms/add.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import AddPost from '/components/Cms/AddPost';
-import AppLayout from '/components/default/layout';
-
-const AddPostPage = () => <AppLayout title={'Add Post'} hideBanner> <AddPost /> </AppLayout>;
-
-export default AddPostPage;
--- pages/admin/cms/posts/[id]/edit.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import EditPost from '/components/Cms/EditPost';
-import AppLayout from '/components/default/layout';
-
-const EditPostPage = () => <AppLayout title={'Edit Post'} hideBanner> <EditPost /> </AppLayout>;
-
-export default EditPostPage;
