5fce8f6d0863050f1e2225debcbd7bad63ea02c2
Francis Kim <francis@80bots.com>
2020-06-19 14:16:37 +0300

    Deleted page Dashboard

--- pages/dashboard.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Dashboard from '/components/Dashboard';
-import AppLayout from '/components/default/layout';
-
-const DashboardPage = () => <AppLayout title={'Dashboard'}> <Dashboard/> </AppLayout>;
-
-export default DashboardPage;
