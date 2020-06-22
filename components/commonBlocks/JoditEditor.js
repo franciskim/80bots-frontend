506b5acfcb2ea585e5fa66cadf47790cd7849b6b
Francis Kim <francis@80bots.com>
2020-06-22 19:39:48 +0300

    remove-cms/deleted Static jodit-editor

--- components/commonBlocks/JoditEditor.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import dynamic from "next/dynamic";
-import("jodit/build/jodit.min.css");
-
-const JoditEditorReact = dynamic(import("jodit-react"), { ssr: false });
-
-export const JoditEditor = ({ value, onChange, config }) => {
-  return <JoditEditorReact value={value} config={config} onChange={onChange} />;
-};
